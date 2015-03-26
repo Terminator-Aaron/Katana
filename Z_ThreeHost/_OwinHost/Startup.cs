987852bbff7880bfb6a5241931568575daae64db
zhangshaohui <terminator_aaron@hotmail.com>
2015-03-26 20:37:34 +0800

    add owinhost demo

---
+++ Z_ThreeHost/_OwinHost/Startup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(_OwinHost.Startup))]
+
+namespace _OwinHost
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            app.Run(context =>
+            {
+                //context.Response.ContentType = "plain/html";
+                return context.Response.WriteAsync("Hello World!");
+            });
+        }
+    }
+}
